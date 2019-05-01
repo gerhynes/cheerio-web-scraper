f83cc5d550e7ce76a28a2259179d083e233679d8
Gerard Hynes <gkhynes@gmail.com>
2019-05-01 22:53:20 +0100

    Set up files

---
+++ scrape.js
@@ -0,0 +1,26 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+request("https://blog.getbootstrap.com/", (error, response, html) => {
+  if (!error && response.statusCode === 200) {
+    const $ = cheerio.load(html);
+
+    const post = $(".post");
+
+    // console.log(post.html());
+    // console.log(post.text());
+
+    // const output = post.find("h1").text();
+    // const output = post.children("h1").text();
+    // const output = post
+    //   .children("h1")
+    //   .next()
+    //   .text();
+    const output = post
+      .children("h1")
+      .parent()
+      .text();
+
+    console.log(output);
+  }
+});
